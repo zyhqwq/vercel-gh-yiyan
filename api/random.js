3f25a4b376853f2e77a72f59f10b011cc1718931
zyhqwq <616950584@qq.com>
2025-10-05 19:49:49 +0800

    修改代码

--- api/random.js
+++ api/random.js
@@ -1,16 +1,11 @@
-// 引入本地的数据文件
-const yiyanData = require('./yiyan.json');
+const yiyanData = require('../yiyan.json');
 
 module.exports = (req, res) => {
-  // 设置响应头，允许跨域访问
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Content-Type', 'text/plain; charset=utf-8');
   
-  // 将字符串按逗号分割成数组
-  const quotesArray = yiyanData.split(',');
-  // 随机选择一条
-  const randomQuote = quotesArray[Math.floor(Math.random() * quotesArray.length)];
+  const randomIndex = Math.floor(Math.random() * yiyanData.length);
+  const randomQuote = yiyanData[randomIndex];
   
-  // 返回随机一言
   res.send(randomQuote);
 };
